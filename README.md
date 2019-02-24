fc219943eacd8989483c3941041099c4a1857c64
xiaohong <xiaohong@xxx.com>
2019-02-24 13:45:11 +0800

    ok

---
+++ README.md
@@ -0,0 +1,7 @@
+##方法如下：
+
+创建springboot项目，在resources/目录下创建名为banner.txt的文件
+##生成文字网址
+浏览器打开网址：http://www.network-science.de/ascii/
+
+##然后 run即可。
---
+++ src/main/java/cn/nice/image_words/ImageWordsApplication.java
@@ -0,0 +1,13 @@
+package cn.nice.image_words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageWordsApplication.class, args);
+	}
+
+}
